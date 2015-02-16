a055c06d25cbf0f221530752de5f839c3e5be61d
Lars Schulna <kartoffelbrei.mit.muskatnuss@gmail.com>
2015-02-17 00:26:41 +0100

    add test project, make build.gpr an aggregate

---
+++ test/run_test.adb
@@ -0,0 +1,9 @@
+with Directed_Graph;
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Run_Test is
+   package Test_Graph is new Directed_Graph(Integer, Integer);
+begin
+   Put("Penus");
+end Run_Test;
